1a406193f374e13b62d82b2f6fd9e5c48a80c5ce
Andrew Cai <caiac@amazon.com>
2023-04-18 17:27:22 +0000

    check postprocessing output in s3 is nonempty

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_smppy.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_smppy.py
@@ -12,16 +12,19 @@
 # permissions and limitations under the License.
 from __future__ import absolute_import
 
+import boto3
+import os
+import time
+
 import pytest
 from sagemaker import utils
 from sagemaker.debugger import ProfilerConfig, SMProfile
-from sagemaker.pytorch import PyTorch
 
 from ...integration import training_dir, smppy_mnist_script, DEFAULT_TIMEOUT
 from ...integration.sagemaker.timeout import timeout
 from . import invoke_pytorch_estimator
 
-INSTANCE_TYPE = 'ml.g4dn.12xlarge'
+INSTANCE_TYPE = "ml.g4dn.12xlarge"
 
 
 @pytest.mark.usefixtures("feature_smppy_present")
@@ -34,20 +37,50 @@ INSTANCE_TYPE = 'ml.g4dn.12xlarge'
 def test_training_smppy(framework_version, ecr_image, sagemaker_regions):
     with timeout(minutes=DEFAULT_TIMEOUT):
         estimator_parameters = {
-            'entry_point': smppy_mnist_script,
-            'role': 'arn:aws:iam::920076894685:role/SageMakerRole',
-            'instance_count': 2,
-            'instance_type': INSTANCE_TYPE,
-            'framework_version': framework_version,
-            'hyperparameters': {'epochs': 1},
-            'profiler_config': ProfilerConfig(
-                smprofile_params = SMProfile(cpu_profiling_duration=3600)
+            "entry_point": smppy_mnist_script,
+            "role": "arn:aws:iam::920076894685:role/SageMakerRole",
+            "instance_count": 2,
+            "instance_type": INSTANCE_TYPE,
+            "framework_version": framework_version,
+            "hyperparameters": {"epochs": 1},
+            "profiler_config": ProfilerConfig(
+                smprofile_params=SMProfile(cpu_profiling_duration=3600)
             ),
-            'debug_hook_config': False,
+            "debug_hook_config": False,
         }
-        upload_s3_data_args = {
-            'path': training_dir,
-            'key_prefix': 'pytorch/mnist'
-        }
-        job_name=utils.unique_name_from_base('test-pt-smppy-training')
-        invoke_pytorch_estimator(ecr_image, sagemaker_regions, estimator_parameters, upload_s3_data_args=upload_s3_data_args, job_name=job_name)
+        upload_s3_data_args = {"path": training_dir, "key_prefix": "pytorch/mnist"}
+        job_name = utils.unique_name_from_base("test-pt-smppy-training")
+        pytorch, _ = invoke_pytorch_estimator(
+            ecr_image,
+            sagemaker_regions,
+            estimator_parameters,
+            upload_s3_data_args=upload_s3_data_args,
+            job_name=job_name,
+        )
+        s3 = boto3.client("s3")
+        bucket = pytorch.output_path.replace("s3://", "").rstrip("/")
+        prefix = _get_detailed_profiler_output_prefix(pytorch)
+
+        # Give postprocessing rule some time to complete
+        time.sleep(60)
+
+        contents = s3.list_objects_v2(Bucket=bucket, Prefix=prefix).get("Contents")
+        assert len(contents) > 0
+        for file in contents:
+            assert file.get("Size") > 0
+
+
+def _get_detailed_profiler_output_prefix(estimator):
+    config_name = None
+    for processing in estimator.profiler_rule_configs:
+        params = processing.get("RuleParameters", dict())
+        rule = config_name = params.get("rule_to_invoke", "")
+        if rule == "DetailedProfilerProcessing":
+            config_name = processing.get("RuleConfigurationName")
+            break
+    return os.path.join(
+        estimator.latest_training_job.name,
+        "rule-output",
+        config_name,
+        "",
+    )
